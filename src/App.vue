7d308fcad8ceacfaf7325758c5c7b053ed218412
Andrey Medvedev <gifteddiz@gmail.com>
2025-10-12 19:49:49 +0500

    Добавлен бутсрап

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <h1>Стартуем тут</h1>
+  <div class="container mt-4">
+    <h1 class="text-primary">Стартуем тут с Bootstrap!</h1>
+    <button class="btn btn-success">Это кнопка Bootstrap</button>
+    <div class="alert alert-info mt-3" role="alert">Простое информационное уведомление!</div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// Your script logic here
+</script>
